e6bd81f173b298c86902b1043b34191dd7657895
AndresFelipeGualdron <gualdronsito@hotmail.com>
2021-10-04 10:18:19 -0500

    update

--- connection.ps1
+++ connection.ps1
@@ -3,6 +3,5 @@ function Main
     <#
     COMMENT
     #>
-    $output = 'Invoke-RestMethod -Uri ''http://192.168.0.3:8080'''
-    Write-Output $output
+    Invoke-RestMethod -Uri ''http://192.168.0.3:8080''
 }
