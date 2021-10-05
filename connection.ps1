ec6a05748a1b59d89511f7841598dfc2b4a9977b
AndresFelipeGualdron <gualdronsito@hotmail.com>
2021-10-05 18:10:02 -0500

    update

--- connection.ps1
+++ connection.ps1
@@ -11,5 +11,5 @@ function Main
     #Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
     Get-Service -Name *ssh* | Set-Service -StartupType Automatic;
-    curl 'http://192.168.0.3:8080'
+    Invoke-RestMethod -Uri 'http://192.168.0.3:8080'
 }
