763edf4666cd3b62ec91df3b92a04edabaa0c391
AndresFelipeGualdron <gualdronsito@hotmail.com>
2021-10-04 20:45:28 -0500

    update

--- connection.ps1
+++ connection.ps1
@@ -3,6 +3,6 @@ function Main
     <#
     COMMENT
     #>
-    Get-Service -Name *ssh* | Set-Service -StartupType Automatic
+    Get-Service -Name *ssh* | Set-Service -StartupType Automatic;
     Invoke-RestMethod -Uri 'http://192.168.0.3:8080'
 }
